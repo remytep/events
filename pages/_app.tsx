b8e8508fea93183c693e7f823ba4660321382406
tepremy <remymtep@gmail.com>
2023-01-19 17:16:19 +0100

    merge event list with login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,7 +29,6 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <AuthProvider>
         <Layout>
-          <Navbar />
           <Component {...pageProps} />
         </Layout>
       </AuthProvider>
